44f1107c9a6ffbacbef2a6a34318371d0a7a5915
bosspichapong <42568992+bosspichapong@users.noreply.github.com>
2020-12-17 18:08:04 +0700

    Add forms

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,11 +1,27 @@
 import { BrowserRouter, Route, Link, Router, Redirect } from 'react-router-dom';
 import { Component } from 'react';
-
+import './RegisterPage.css'
 
 class Register extends Component {
   render() {
     return (
-      <span>hooooooo</span>
+      <form>
+        {/* <!-- 2 column grid layout with text inputs for the first and last names --> */}
+        <div class="row mb-4">
+          <div class="col">
+            <div class="form-outline">
+              <input type="text" id="form3Example1" class="form-control" />
+              <label class="form-label" for="form3Example1">First name</label>
+            </div>
+          </div>
+          <div class="col">
+            <div class="form-outline">
+              <input type="text" id="form3Example2" class="form-control" />
+              <label class="form-label" for="form3Example2">Last name</label>
+            </div>
+          </div>
+        </div>
+      </form>
     );
   }
 }
