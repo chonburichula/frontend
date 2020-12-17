94470717bc8f69e2fa9800269a7f94787d9fb76a
bosspichapong <42568992+bosspichapong@users.noreply.github.com>
2020-12-17 15:26:29 +0700

    Add git ignore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './res/logo.jpg';
 import './App.css';
-
 import { BrowserRouter, Route, Link, Router, Redirect } from 'react-router-dom';
+
 import { Component } from 'react';
 
 import Home from './components/Home'
@@ -11,13 +10,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Link to="/">Home</Link>
-          <Link to="/register">register</Link>
-          <Route exact path="/" component={Home} />
-          <Route path="/register" component={Register} />
-        </header>
+        <Route exact path="/" component={Home} />
+        <Route path="/register" component={Register} />
       </div>
     );
   }
