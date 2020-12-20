6b20aa72b3202e7927da2779f5aa2d55016fdef5
Theerapat Kangsilalai <tkangsilalai@gmail.com>
2020-12-20 18:15:41 +0700

    Trying Adaptive Layout

--- src/App.js
+++ src/App.js
@@ -3,10 +3,57 @@ import { BrowserRouter, Route, Link, Router, Redirect } from 'react-router-dom';
 
 import { Component } from 'react';
 
-import Home from './components/Home'
+/*import Home from './components/Home'*/
 import Register from './components/RegisterPage'
 
-class App extends Component {
+import React from "react";
+
+import Global from "./styles/global";
+
+import Home from "./pages/Home/Home";
+
+const lorem =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, sed iure blanditiis voluptatum nulla quidem minus quam tempora obcaecati necessitatibus inventore! Vitae totam quam pariatur facilis fugit maxime adipisci eaque.";
+
+const data = [
+  {
+    id: Math.random(),
+    title: "Box titulo 1",
+    text: lorem,
+    bgColor: "#D5CAFA"
+  },
+  {
+    id: Math.random(),
+    title: "Box titulo 2",
+    text: lorem,
+    bgColor: "#EDA9A9"
+  },
+  {
+    id: Math.random(),
+    title: "Box titulo 3",
+    text: lorem,
+    bgColor: "#F2EE8D"
+  },
+  {
+    id: Math.random(),
+    title: "Box titulo 4",
+    text: lorem,
+    bgColor: "#9FEACD"
+  }
+];
+
+function App() {
+  return (
+    <>
+      <Global />
+      <Home boxData={data} />
+    </>
+  );
+}
+
+export default App;
+
+/*class App extends Component {
   render() {
     return (
       <div className="App">
@@ -17,4 +64,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App;*/
